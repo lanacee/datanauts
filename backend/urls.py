7cd086388f6987d7d2cbea0259055979e8994d5f
Alana <alana@Alanas-MacBook-Pro.local>
2022-08-20 15:57:31 +1000

    added react route to django app

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("app/",
+    (never_cache(TemplateView.as_view(template_name="index.html"))))
 ]
