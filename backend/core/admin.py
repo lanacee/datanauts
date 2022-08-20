73d5c5ff5d97590cf21bc8820f8c4d5434723aee
Alana <alana@Alanas-MacBook-Pro.local>
2022-08-20 18:16:28 +1000

    added models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PestTrap, Observation
 
 # Register your models here.
+admin.site.register(PestTrap)
+admin.site.register(Observation)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PestTrap(models.Model):
+    # Multi-tenancy model
+    name = models.CharField(max_length=100)
+    UniqueId = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    users = models.ManyToManyField(User)
+
+# Create your models here.
+class Observation(models.Model):
+    # Multi-tenancy model
+    name = models.CharField(max_length=100)
+    UniqueId = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    pestTrap = models.ForeignKey(PestTrap, on_delete=models.CASCADE)
+
+# # Grower subscribes to receive notifications for a particular pesttrap
+# class PestTrapByGrower(models.Model):
+#     pestTrap = models.ForeignKey(PestTrap)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
