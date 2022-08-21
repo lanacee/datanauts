b4d18a7bb18708259bda17d8f503f63f82e6fcc7
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2022-08-21 11:08:04 +1000

    Add serializer for pesttrap and also proxy for local dev

---
+++ backend/core/serializers.py
@@ -0,0 +1,33 @@
+from .models import PestTrap, Observation
+from rest_framework import serializers
+
+class PestTrapSerializer(serializers.ModelSerializer):
+    # Append current user to API request, so you don't need to directly fetch user id.
+    # See discussion https://stackoverflow.com/a/64991320
+    def create(self, validated_data):
+
+        request = self.context.get("request")
+
+        pest_trap = PestTrap()
+        pest_trap.name = validated_data["name"]
+        pest_trap.uniqueId = validated_data["uniqueId"]
+        pest_trap.description = validated_data["description"]
+        pest_trap.user = request.user
+
+        pest_trap.save()
+
+        return pest_trap
+
+    class Meta:
+        """
+        https://www.django-rest-framework.org/api-guide/serializers/#modelserializer
+        Also in tutorial, note that a Serializer is like a Form class, whereas a ModelSerializer
+        is like a ModelForm because it maps to a class.
+        https://www.django-rest-framework.org/tutorial/1-serialization/#using-modelserializers
+        """
+
+        model = PestTrap
+        # https://www.django-rest-framework.org/api-guide/relations/
+
+        exclude = ["user"]
+        extra_kwargs = {"user": {"required": False}}
